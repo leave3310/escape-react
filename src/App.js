29bd75c5e5abb20de61f5afad9dc41be3a6d0fd7
leave3310 <sampisam3@gmail.com>
2022-04-02 18:24:19 +0800

    Adaxios in Article and ArticleDetail

--- src/App.js
+++ src/App.js
@@ -7,17 +7,19 @@ import './App.css';
 import Nav from './components/Nav';
 import Products from './pages/Products';
 import ProductDetail from "./pages/ProductDetail";
+import Article from './pages/Article'
+import ArticleDetail from './pages/ArticleDetail'
 
 function App() {
-  
+
   return (
     <BrowserRouter>
-      <Nav/>
+      <Nav />
       <Routes>
         <Route path="products" element={<Products />} />
         <Route path="products/:productId" element={<ProductDetail />} />
-        <Route path="article" />
-        <Route path="article/:articleId" />
+        <Route path="article" element={<Article />} />
+        <Route path="article/:articleId" element={<ArticleDetail />} />
       </Routes>
     </BrowserRouter>
   )
--- src/pages/ArticleDetail.jsx
+++ src/pages/ArticleDetail.jsx
@@ -1,47 +1,80 @@
-<div className="container">
-  <div className="row">
-    <div className="col-md-6">
-      <div className="card">
-        <div className="card-header bg-white">
-          <h5 className="card-title">文章內容</h5>
-        </div>
-        <div className="card-body">
-          <form>
-            <div className="mb-3">
-              <label htmlFor="title" className="form-label">文章名稱</label>
-              <input type="text" className="form-control" id="title" />
+import react, { useState, useEffect } from "react"
+import axios from "axios"
+import { useParams } from "react-router-dom"
+
+const ArticleDetail = () => {
+  const [article, setArticle] = useState({})
+  const { articleId } = useParams()
+
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_URL}api/react/article/${articleId}`)
+      .then(res => {
+        setArticle(res.data.article)
+      })
+  }, [])
+
+  const handleChange = (event) => {
+    const { id, value } = event.target;
+    setArticle({
+      ...article,
+      [id]: value
+    })
+  }
+
+  const clearTag = () => {
+    setArticle({
+      ...article,
+      tag: ''
+    })
+  }
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          <div className="card">
+            <div className="card-header bg-white">
+              <h5 className="card-title">文章內容</h5>
             </div>
-            <div className="mb-3">
-              <label htmlFor="author" className="form-label">文章作者</label>
-              <input type="text" className="form-control" id="author" />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="content" className="form-label">文章描述</label>
-              <textarea className="form-control" id="content" />
-            </div>
-            <div className="row gx-1 mb-3">
-              <div className="col-md-2 mb-1">
-                <div className="input-group input-group-sm">
-                  <input type="text" className="form-control form-control" id="tag" placeholder="請輸入標籤" />
-                  <button type="button" className="btn btn-outline-danger">x</button>
+            <div className="card-body">
+              <form>
+                <div className="mb-3">
+                  <label htmlFor="title" className="form-label">文章名稱</label>
+                  <input type="text" className="form-control" id="title" value={article.title} onChange={handleChange} />
                 </div>
-              </div>
+                <div className="mb-3">
+                  <label htmlFor="author" className="form-label">文章作者</label>
+                  <input type="text" className="form-control" id="author" value={article.author} onChange={handleChange} />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="content" className="form-label">文章描述</label>
+                  <textarea className="form-control" id="content" value={article.description} onChange={handleChange} />
+                </div>
+                <div className="row gx-1 mb-3">
+                  <div className="col-md-2 mb-1">
+                    <div className="input-group input-group-sm">
+                      <input type="text" className="form-control form-control" id="tag" placeholder="請輸入標籤" value={article.tag} onChange={handleChange} />
+                      <button type="button" className="btn btn-outline-danger" onClick={clearTag}>x</button>
+                    </div>
+                  </div>
+                </div>
+              </form>
             </div>
-          </form>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <div className="card">
+            <div className="card-header bg-white">
+              <h5 className="card-title">文章圖片</h5>
+            </div>
+            <div className="card-body">
+              <img src={article.image} alt={article.image} className="rounded mb-2 me-2" style={{ width: 150 }} />
+            </div>
+          </div>
         </div>
       </div>
     </div>
-    <div className="col-md-6">
-      <div className="card">
-        <div className="card-header bg-white">
-          <h5 className="card-title">文章圖片</h5>
-        </div>
-        <div className="card-body">
-          ...
-        </div>
-      </div>
-    </div>
-  </div>
+  )
+}
 
-
-</div>
+export default ArticleDetail
